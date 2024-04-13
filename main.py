b487802f96f9f2820a534c01f235b3b1704ec9b5
SumitRodrigues <sumitrod11@gmail.com>
2024-04-12 20:35:21 -0700

    user agent and driver setup

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ notification.send()
 
 options = Options()
 # options.add_argument("--headless")
+# options.add_argument("--proxy-server=gate.nodemaven.com:8080")
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+
+with open("products.txt") as f:
+    products = f.readlines
+
+
+driver = webdriver.Chrome(options=options)
+
+for product in products:
+    driver.get(product)
