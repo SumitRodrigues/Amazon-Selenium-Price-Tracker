1e3242ed3b9ab35adb91ea97e36137b198d203aa
SumitRodrigues <sumitrod11@gmail.com>
2024-04-12 20:46:05 -0700

    populating web pages

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ options = Options()
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 
 with open("products.txt") as f:
-    products = f.readlines
+    products = f.readlines()
 
 
 driver = webdriver.Chrome(options=options)
 
+i = 0
 for product in products:
     driver.get(product)
+    i += 1
+    page_source = driver.page_source
+    with open(f"data/{i}.html", "w", encoding="utf-8") as f:
+        f.write(page_source)
